88e8accfe83610520e1d99e985b2cfa681d8169a
Gihan Sandaru <w.g.sandaru@gmail.com>
2019-01-27 23:26:37 -0800

    bottomsheet dialog added

--- app/src/main/java/com/example/gihansandaru/eventbussample/MainActivity.java
+++ app/src/main/java/com/example/gihansandaru/eventbussample/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.gihansandaru.eventbussample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout bottomSheet;
+    private BottomSheetBehavior sheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TestFragment fragment = new TestFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+
+
+        bottomSheet = findViewById(R.id.bottom_sheet);
+        sheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        sheetBehavior.setHideable(false);
+
+
     }
 }
